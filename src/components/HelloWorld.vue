4bf48f0e8be0b5eac8d9b8ac11c0f266b26d9e35
wangyongjie <xiaodongxier@gmail.com>
2023-08-01 06:37:46 +0800

    tidi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,46 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+  <div class="todo">
+    <h2>todo</h2>
+    <ol>
+      <todo-item v-for="item in itemList" :key="item.id" :itemprop="item"></todo-item>
+    </ol>
   </div>
 </template>
 
 <script>
+import TodoItem from './demo/demo1-todoitem.vue'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      itemList: [
+        {
+          id: '0',
+          title: '事件1'
+        },
+        {
+          id: '1',
+          title: '事件2'
+        },
+        {
+          id: '3',
+          title: '事件3'
+        },
+        {
+          id: '1',
+          title: '事件3'
+        }
+      ]
     }
+  },
+  components: {
+    TodoItem
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
+
+
---
+++ src/components/demo/demo1-todoitem.vue
@@ -0,0 +1,13 @@
+<template>
+  <li> {{ itemprop.title }}</li>
+</template>
+
+<script>
+export default {
+props: ['itemprop']
+}
+</script>
+
+<style>
+
+</style>
