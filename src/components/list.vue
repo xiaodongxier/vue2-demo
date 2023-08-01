b3538bfb2a7cbe292141c147ada3e7bbf0b6e8b9
wangyongjie <xiaodongxier@gmail.com>
2023-08-01 08:49:47 +0800

    feat: 插槽

---
+++ src/components/list.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button>{{ title }}</button>
+    <a :href="url" target="_blank">连接</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'list',
+  props: {
+    title: String,
+    url: String
+  },
+  data() {
+    return {
+    }
+  }
+}
+</script>
+<style lang='' scoped>
+  
+  
+  
+  
+</style>
