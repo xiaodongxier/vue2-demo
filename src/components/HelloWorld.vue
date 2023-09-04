29759daf5d7b4f295b25a518ad81cadd7877f88e
wangyongjie <xiaodongxier@gmail.com>
2023-09-04 09:35:51 +0800

    传参

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="hello">
-    {{ msg }}
+   <h1>首页</h1>
+   <br>
+    <router-link to="/PageOne/111">PageOne</router-link> |
+    <router-link :to="{name: 'PageOne'}">PageOne - name</router-link> |
+
+    <router-link :to="{name: 'PageOne', params: {id1: 123,age: 18}}">PageOne - name + params</router-link> |
+    <router-link :to="{name: 'PageOne', query: {id: 456, age: 28}}">PageOne - name + query</router-link> ｜
+
+    <router-link :to="{path: 'PageOne', params: {id1: 123,age: 18}}">PageOne - path + params</router-link> |
+    <router-link :to="{path: 'PageOne', query: {id: 456, age: 28}}">PageOne - path + query</router-link> ｜
   </div>
 </template>
 
---
+++ src/components/PageOne.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>PageOne</h1>
+    <router-link to="/">首页</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PageOne',
+  data() {
+    return {
+
+    }
+  },
+  mounted(){
+    console.log("this.$route", this.$route)
+    console.log("this.$route.params", this.$route.params)
+    console.log("this.$route.query", this.$route.query)
+  }
+}
+</script>
+<style lang="" scoped>
+  
+</style>
